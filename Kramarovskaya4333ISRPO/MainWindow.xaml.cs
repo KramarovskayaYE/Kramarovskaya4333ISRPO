b3ce86a9c9ad88a1dafe98842004aa66f9ed0e7e
yana <kramarovskaaana@gmail.com>
2026-02-07 16:17:09 +0300

    Добавлен обработчик события для кнопки

--- Kramarovskaya4333ISRPO/MainWindow.xaml.cs
+++ Kramarovskaya4333ISRPO/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Kramarovskaya4333ISRPO
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Кнопка нажата!");
+        }
     }
 }
