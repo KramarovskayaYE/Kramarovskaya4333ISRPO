c2e041b353d0d9f177bc5f3828aa50805ad8acb5
yana <kramarovskaaana@gmail.com>
2026-02-07 16:07:13 +0300

    Добавлено новое окно new_windows

---
+++ Kramarovskaya4333ISRPO/new_windows.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kramarovskaya4333ISRPO
+{
+    /// <summary>
+    /// Логика взаимодействия для new_windows.xaml
+    /// </summary>
+    public partial class new_windows : Window
+    {
+        public new_windows()
+        {
+            InitializeComponent();
+        }
+    }
+}
